f5556855343879ad1d5665f1011ccafbf602af0c
Andrey Kalmykov <andrey.kalmykov@sibers.com>
2022-07-05 18:26:24 +0700

    Add es2021 last futures

---
+++ finalizationRegistry.js
@@ -0,0 +1,21 @@
+/*
+const registry = new FinalizationRegistry((value) => {
+  console.log('-> print value: ', value);
+});
+
+(function () {
+  const obj = {};
+
+  registry.register(obj, "Object deleted");
+})();
+
+(async () => {
+  try {
+    await Promise.any([Promise.reject("Error 1")]);
+  } catch (e) {
+    console.log('-> print error ', e.errors);
+  }
+})()
+*/
+
+
